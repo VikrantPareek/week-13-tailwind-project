5e90c23ff8998b20290dbbfe8f7ca139707fe26a
Vikrant Pareek <vikrantpareek2005@gmail.com>
2026-05-04 15:27:50 +0530

    Created CalenderMeeting component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { CalenderDate } from "./components/CalenderDate";
+import { CalenderMeeting } from "./components/CalenderMeeting";
 import { ProfileCard } from "./components/ProfileCard";
 import { ProfileIcon } from "./components/ProfileIcon";
 import { SidebarOption } from "./components/SidebarOption";
 import WebinarButton from "./components/WebinarButton";
 
 function App() {
-  return <CalenderDate date={"Monday, 04 May 2026"}/>
+  return <CalenderMeeting time={"11:30 AM"} status={"Live"} topic={"UI/UX"} />
 }
 
 export default App
