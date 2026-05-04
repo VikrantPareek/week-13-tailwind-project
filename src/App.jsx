16c53e50ffe0703b3320ee8c4120e7797279e560
Vikrant Pareek <vikrantpareek2005@gmail.com>
2026-05-04 15:11:19 +0530

    Created CalenderDate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { CalenderDate } from "./components/CalenderDate";
 import { ProfileCard } from "./components/ProfileCard";
 import { ProfileIcon } from "./components/ProfileIcon";
 import { SidebarOption } from "./components/SidebarOption";
 import WebinarButton from "./components/WebinarButton";
 
 function App() {
-  return <ProfileCard name={"Kitty Delhi"} mail={"kitty@gmail.com"} mobile={"+91 1515515515"} location={"Delhi, India"}/>
+  return <CalenderDate date={"Monday, 04 May 2026"}/>
 }
 
 export default App
