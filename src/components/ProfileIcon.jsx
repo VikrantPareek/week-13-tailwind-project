7bf1c7d4a45657d047b60c2b2cb22d0886eaf6f1
Vikrant Pareek <vikrantpareek2005@gmail.com>
2026-05-04 14:48:08 +0530

    Created ProfileCard component

--- src/components/ProfileIcon.jsx
+++ src/components/ProfileIcon.jsx
@@ -2,7 +2,7 @@ export function ProfileIcon(props) {
   return (
     <div>
       <img
-        className={`h-${props.height} rounded-lg`}
+        className={`h-${props.height} rounded-lg ${props.className}`}
         src="https://thumbs.dreamstime.com/b/cute-cat-portrait-square-photo-beautiful-white-closeup-105311158.jpg"
         alt="profile-image"
       />
