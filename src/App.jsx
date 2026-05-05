6d2567d72da19295bddc8d8dc7f49e335b9fedc5
Vikrant Pareek <vikrantpareek2005@gmail.com>
2026-05-05 22:43:25 +0530

    Updated the structure

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <SidebarOption
             icon={
               <svg
-                xmlns="http://www.w3.org/2000/svg"
+                xmlns="http://www.w3.org/2000/svg "
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
